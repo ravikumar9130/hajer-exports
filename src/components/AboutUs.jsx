646ba7de12aff8acbb0e821a3915c88d6ed3bf34
ravikumar <ravikumarwolfd9130@gmail.com>
2022-10-03 21:19:32 +0530

    about us ~

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -8,10 +8,10 @@ export default function AboutUs() {
         <img className="rounded-lg mx-8 w-full md:w-2/5" src={about} alt="about us" />
 
         <div className="mt-4 mx-auto">
-          <div className="text-4xl w-full  text-center  text-primary  pb-8">
+          <div className="text-4xl w-full  text-center  text-primary pb-4  md:pb-8">
             About Us
                   </div>
-                  <div className="md:text-center text-xl"> We, Hajer Exports, situated at address..... are a leading supplier of
+                  <div className="md:text-center md:text-xl"> We, Hajer Exports, situated at address..... are a leading supplier of
           first class stone around the world. We stock a wide variety of natural
           stone slabs and tiles. The wide range of stone product includes
           granite, marble and many more. We are always on the leading edge of
